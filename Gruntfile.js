54cb95d6f1e79049009c05a55c70901dd084ba6a
Jeffrey Lembeck <jlembeck@gmail.com>
2014-05-23 15:35:03 -0700

    Big restructuring of files.
    Added unit testing capabilities. Added travis CI. Added minification.

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+/*global module:false*/
+/*global require:true*/
+module.exports = function(grunt) {
+	'use strict';
+
+	// Project configuration.
+	grunt.initConfig({
+
+		// Constants
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+		'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+		'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+		'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+		' Licensed <%= pkg.license %> */\n',
+		name: 'fixedfixed',
+
+		// Task Configuration
+		jshint: {
+			src: {
+				options: {
+					jshintrc: '.jshintrc'
+				},
+				src: 'src/<%= name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>'
+			},
+			dist: {
+				src: 'src/<%= name %>.js',
+				dest: 'dist/<%= name %>.min.js'
+			}
+		},
+		qunit: {
+			all: ['test/**/*.html']
+		}
+	});
+
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+
+	// Default task.
+	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
+	grunt.registerTask( 'lint', [ 'jshint' ] );
+	grunt.registerTask( 'default', [ 'jshint', 'qunit', 'uglify' ] );
+};
