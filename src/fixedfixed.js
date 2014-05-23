89d52545a52317a46c45eced58d5e3f6af2f36b3
Jeffrey Lembeck <jlembeck@gmail.com>
2014-05-23 15:49:02 -0700

    Trailing whitespace

--- src/fixedfixed.js
+++ src/fixedfixed.js
@@ -1,11 +1,11 @@
 /*! Fixedfixed: a CSS position:fixed qualifier. (c)2012 @scottjehl, Filament Group, Inc. Dual license: MIT and/or GPLv2 */
 (function( w, undefined ){
-	
+
 	var htmlclass = "fixed-supported",
 		el = w.document.createElement( "div" ),
 		ua = w.navigator.userAgent,
 		docEl = w.document.documentElement;
-	
+
 	// fix the test element
 	el.style.position = "fixed";
 	el.style.top = 0;
@@ -17,7 +17,7 @@
 
 		// only run test if there's a scroll we can compare
 		if( scroll !== undefined && scroll > 0 && w.document.body ){
-			
+
 			w.document.body.insertBefore( el, w.document.body.firstChild );
 
 			if( !el.getBoundingClientRect || el.getBoundingClientRect().top !== 0 ){
@@ -27,7 +27,7 @@
 
 			// remove the test element
 			w.document.body.removeChild( el );
-				
+
 			// unbind the handlers
 			if( w.removeEventListener ){
 				w.removeEventListener( "scroll", checkFixed, false );
@@ -35,9 +35,9 @@
 			else{
 				w.detachEvent( "onscroll", checkFixed );
 			}
-		}		
+		}
 	}
-		
+
 	// if a particular UA is known to return false results with this feature test, try and avoid that UA here.
 	if(
 		// Android 2.1, 2.2, 2.5, and 2.6 Webkit
@@ -52,7 +52,7 @@
 	){
 		//add the HTML class for now.
 		docEl.className += " " + htmlclass;
-		
+
 		// bind to scroll event so we can test and potentially degrade
 		if( w.addEventListener ){
 			w.addEventListener( "scroll", checkFixed, false );
